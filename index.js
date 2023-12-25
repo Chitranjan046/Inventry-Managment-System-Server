2d8bfaaac1971715c5919b15563b5a406352f69b
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-12-25 12:01:28 +0530

    Database connection don

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
